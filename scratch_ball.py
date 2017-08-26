589e6b45bda8943f064e372ebeeeec0389633bc4
ancientpuma <17Vuyjdtybq>
2017-08-26 08:56:26 +0300

    Первый блин 2

--- scratch_ball.py
+++ scratch_ball.py
@@ -1,12 +1,13 @@
 import tkinter
 
 def print_hello(event):
-    print(event)
+    print('5')
 
 root = tkinter.Tk()
 
 button = tkinter.Button(root, text = "Button")
 button.bind("<Button-1>", print_hello)
+
 button.pack
 
 root.mainloop()
